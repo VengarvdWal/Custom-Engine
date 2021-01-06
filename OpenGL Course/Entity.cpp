84e3d73c6296804b8d00f13644de48bd271ff87c
VengarvdWal <vengar_live@hotmail.com>
2021-01-06 15:14:01 +0100

    First Building Version

--- OpenGL Course/Entity.cpp
+++ OpenGL Course/Entity.cpp
@@ -1,19 +1,22 @@
 #include "Entity.h"
 
-Entity::Entity(String modelPath)
+Entity::Entity()
 {
-	model.loadModel(modelPath);
 	mHealth = 0;
 	mDamage = 0;
 	mSpeed = 0;
 }
 
-//This constructor calls the default constructor first to initialize the model and then sets the values.
-Entity::Entity(String modelPath, int mHealth, int mDamage, int mSpeed) : Entity(modelPath)
+Entity::Entity(int mHealth, int mDamage, int mSpeed)
 {
 	this->mHealth = mHealth;
 	this->mDamage = mDamage;
 	this->mSpeed = mSpeed;
+}
+
+Entity::Render() 
+{	
+	model.RenderModel();
 }
 
 Entity::~Entity()
